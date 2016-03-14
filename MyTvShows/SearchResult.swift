c53b237934440d59dc108b02d2e04deb3391fc19
romainhild <hild.romain@gmail.com>
2016-03-14 22:28:12 +0100

    perform asynchronous search to TvDB

---
+++ MyTvShows/SearchResult.swift
@@ -0,0 +1,19 @@
+//
+//  SearchResult.swift
+//  MyTvShows
+//
+//  Created by Romain Hild on 14/03/2016.
+//  Copyright © 2016 Romain Hild. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var id = 0
+    var name = ""
+    var lang = ""
+}
+
+func < (lhs: SearchResult, rhs: SearchResult) -> Bool {
+    return lhs.name.localizedStandardCompare(rhs.name) == .OrderedAscending
+}
---
+++ MyTvShows/SearchViewController.swift
@@ -0,0 +1,184 @@
+//
+//  ViewController.swift
+//  MyTvShows
+//
+//  Created by Romain Hild on 14/03/2016.
+//  Copyright © 2016 Romain Hild. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    var language = ""
+
+    var searchResults = [SearchResult]()
+    var hasSearched = false
+    
+    var currentElementParsed = ""
+    
+    var dataTask: NSURLSessionDataTask?
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // put the content of the tableview below the searchbar
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0,
+            right: 0)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func urlWithSearchText(searchText: String) -> NSURL {
+        let escapedSearchText = searchText.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
+        var languageQuery = ""
+        if !language.isEmpty {
+            languageQuery = "&language=\(language)"
+        }
+        let urlString = String(format: "https://thetvdb.com/api/GetSeries.php?seriesname=%@%@", escapedSearchText, languageQuery)
+        let url = NSURL(string: urlString)
+        return url!
+    }
+    
+    func parseXML(data: NSData) -> Bool {
+        let parser = NSXMLParser(data: data)
+        parser.delegate = self
+        return parser.parse()
+    }
+    
+    func showNetworkError() {
+        let alert = UIAlertController(title: "Whoops...", message: "There was an error reading from the TvDB. Please try again.", preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        if !searchBar.text!.isEmpty {
+            dataTask?.cancel()
+            hasSearched = true
+            searchBar.resignFirstResponder()
+            
+            searchResults = [SearchResult]()
+            
+            let url = urlWithSearchText(searchBar.text!)
+            
+            let session = NSURLSession.sharedSession()
+            
+            dataTask = session.dataTaskWithURL(url ) {
+                data, response, error in
+                if let error = error where error.code == -999 {
+                    return
+                } else if let httpResponse = response as? NSHTTPURLResponse where httpResponse.statusCode == 200 {
+                    print("Success! \(data!)")
+                    if self.parseXML(data!) {
+                        self.searchResults.sortInPlace(<)
+                    
+                        dispatch_async(dispatch_get_main_queue()) {
+                            self.tableView.reloadData()
+                        }
+                        return
+                    } else {
+                        dispatch_async(dispatch_get_main_queue()) {
+                            self.hasSearched = false
+                            self.tableView.reloadData()
+                            self.showNetworkError()
+                        }
+                    }
+                } else {
+                    print("Failure! \(response)")
+                }
+                
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.hasSearched = false
+                    self.tableView.reloadData()
+                    self.showNetworkError()
+                }
+            }
+            
+            dataTask?.resume()
+        }
+        
+    }
+    
+    func positionForBar(bar: UIBarPositioning) -> UIBarPosition {
+        return .TopAttached
+    }
+}
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if !hasSearched {
+            return 0
+        } else if searchResults.count == 0 {
+            return 1
+        } else {
+            return searchResults.count
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "SearchResultCell"
+        
+        var cell: UITableViewCell! = tableView.dequeueReusableCellWithIdentifier(cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .Subtitle, reuseIdentifier: cellIdentifier)
+        }
+        
+        if searchResults.count == 0 {
+            cell.textLabel!.text = "Nothing found"
+            cell.detailTextLabel!.text = ""
+        } else {
+            let searchResult = searchResults[indexPath.row]
+            cell.textLabel!.text = searchResult.name
+            cell.detailTextLabel!.text = searchResult.lang
+        }
+        
+        return cell
+    }
+}
+
+extension SearchViewController: UITableViewDelegate {
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        if searchResults.count == 0 {
+            return nil
+        } else {
+            return indexPath
+        }
+    }
+}
+
+extension SearchViewController: NSXMLParserDelegate {
+    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+        if elementName == "Series" {
+            searchResults.append(SearchResult())
+        }
+        currentElementParsed = elementName
+    }
+    
+    func parser(parser: NSXMLParser, foundCharacters string: String) {
+        if string != "\n" {
+            switch currentElementParsed {
+            case "seriesid":
+                searchResults.last!.id = Int(string)!
+            case "SeriesName":
+                searchResults.last!.name += string
+            case "language":
+                searchResults.last!.lang = string
+            default:
+                break
+            }
+        }
+    }
+}
